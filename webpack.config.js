dc821c9e24b8ace81c111711888614dfd8cb035e
Justin McCandless <justinjmccandless@gmail.com>
2017-12-17 00:27:14 -0800

    Production build works

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,39 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    library: 'tiny-molecule',
+    libraryTarget: 'commonjs2',
+  },
+  externals: {
+    'parse-pdb': {
+      commonjs: 'parse-pdb',
+      commonjs2: 'parse-pdb',
+    },
+    three: {
+      commonjs: 'three',
+      commonjs2: 'three',
+      root: 'THREE',
+    },
+    'three/examples/js/controls/OrbitControls': {
+      commonjs: 'three/examples/js/controls/OrbitControls',
+      commonjs2: 'three/examples/js/controls/OrbitControls',
+    },
   },
   module: {
     rules: [
       {
         test: require.resolve('three/examples/js/controls/OrbitControls'),
         use: ['imports-loader?THREE=THREE', 'exports-loader?THREE.OrbitControls'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
