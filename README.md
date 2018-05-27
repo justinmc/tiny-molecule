342b20f1fce3cd161f6c2651f2d0f5fb5585e03b
Justin McCandless <justin.mccandless@autodesk.com>
2018-05-27 00:58:23 -0700

    3d sphere representation

--- README.md
+++ README.md
@@ -17,7 +17,26 @@ tinyMolecule(pdbString, document.body);
 ## API
 `tinyMolecule` exports a single function that takes a DOM element and a data string.  The data string can be either the contents of a PDB or MMCIF file.  It will synchronously render a canvas into the given DOM element visualizing the given molecule.
 
+```
+tinyMolecule(
+  window.document.body, // the DOM element to render into
+  dataString, // string of pdb or mmcif data
+  {
+    type: 'PDB', // or 'MMCIF' depending on what filetype dataString is
+    representation: 'particle', // or 'sphere'
+  }
+);
+```
+
 See the example folder for a full working example that uses RCSB's API to fetch molecules by PDBID.  You can run it for yourself with `npm run example` (after an `npm install`).
+
+### options
+
+#### type {PDB|MMCIF}
+Indicates the type of data that is being input into tinyMolecule.  Currently PDB and MMCIF files are supported.
+
+#### representation {particle|sphere}
+The fastest option is `particle`, which draws a 2d particle that looks like a sphere for each atom.  `sphere` draws a fully lit 3D sphere for each atom, but is slow for large structures.
 
 ## License
 MIT.  See LICENSE file.
--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ const TYPES = {
   PDB: 'PDB',
   MMCIF: 'MMCIF',
 };
+const REPRESENTATIONS = {
+  PARTICLE: 'particle',
+  SPHERE: 'sphere',
+};
 
 let camera;
 let scene;
@@ -25,10 +29,10 @@ const clock = new THREE.Clock();
  * @param {String} dataString the pdb or mmcif data to display
  * @param {Object} options
  * @param {PDB|MMCIF} options.type
+ * @param {REPRESENTATIONS} options.representation
  */
 function fillScene(dataString, options) {
   scene = new THREE.Scene();
-  const geometry = new THREE.Geometry();
 
   let atoms;
   if (options.type === TYPES.PDB) {
@@ -39,6 +43,7 @@ function fillScene(dataString, options) {
     throw new Error('Please pass a valid options.type; either "PDB" or "MMCIF"');
   }
 
+  const geometry = new THREE.Geometry();
   atoms.forEach((atom) => {
     const vertex = new THREE.Vector3(atom.x, atom.y, atom.z);
     geometry.vertices.push(vertex);
@@ -77,24 +82,43 @@ function fillScene(dataString, options) {
     boundingBox.min.z + ((boundingBox.max.z - boundingBox.min.z) / 2),
   );
 
-  const disk = new THREE.TextureLoader().load('./src/images/sphere.png');
-  const material = new THREE.PointsMaterial({
-    alphaTest: 0.5,
-    size: 5,
-    sizeAttenuation: true,
-    map: disk,
-    transparent: true,
-  });
-  material.color.setHSL(0.9, 0.2, 0.6);
+  // Render using the given representation (default to sphere)
+  if (options.representation === REPRESENTATIONS.PARTICLE) {
+    const disk = new THREE.TextureLoader().load('./src/images/sphere.png');
+    const material = new THREE.PointsMaterial({
+      alphaTest: 0.5,
+      size: 5,
+      sizeAttenuation: true,
+      map: disk,
+      transparent: true,
+    });
+    material.color.setHSL(0.9, 0.2, 0.6);
 
-  // Set the camera's target to the midpoint of the structure
-  cameraControls.target = midpoint;
+    // Set the camera's target to the midpoint of the structure
+    cameraControls.target = midpoint;
 
-  const particles = new THREE.Points(geometry, material);
-  particles.sortParticles = true;
-  scene.add(particles);
+    const particles = new THREE.Points(geometry, material);
+    particles.sortParticles = true;
+    scene.add(particles);
+  } else {
+    const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+    geometry.vertices.forEach((vector3) => {
+      const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+      sphere.position.set(vector3.x, vector3.y, vector3.z);
+      scene.add(sphere);
+    });
+  }
 
-  camera.position.set(0, 0, boundingSphereRadius * 1.5);
+  const cameraPosition = new THREE.Vector3(0, 0, boundingSphereRadius * 1.5);
+  camera.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+
+  // TODO make light move around with camera?
+  const ambientLight = new THREE.AmbientLight(0x222222);
+  scene.add(ambientLight);
+  const light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
+  light.position.set(cameraPosition.x, cameraPosition.y, cameraPosition.z);
+  scene.add(light);
 }
 
 function init() {
