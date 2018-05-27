389ea20fd7da6e124eb3f3a673cdfc750ed4a452
Justin McCandless <justin.mccandless@autodesk.com>
2018-05-27 00:00:37 -0700

    Github pages setup. Will be on gh-pages branch

--- webpack.example.config.js
+++ webpack.example.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './example/app.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './'),
     filename: 'example.bundle.js',
   },
   devtool: 'inline-source-map',
